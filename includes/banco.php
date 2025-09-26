bd0948cb358847c471a49d8809aba4c06bc3a695
Joao Barros <joaopilzbarros@gmail.com>
2025-09-26 11:51:06 -0300

    alterações

--- includes/banco.php
+++ includes/banco.php
@@ -1,17 +1,18 @@
 <pre><?php
-	$banco = new mysqli("localhost", "root", "", "bd_games");
+	$banco = new mysqli("localhost", "root", "", "bd_games"); //  host, user, senha, banco
+	// testar a conexao
 	if($banco->connect_errno){
 		echo "<p>Encontrei um erro $banco -> errno --> $banco->connect_error</p>";
 		die();
 	}
-	
-	$busca = $banco->query("select * from generos");
-	if(!$busca) {
-		echo "<p>A busca não aconteceu</p>";
-	}
-	else{
-		while($reg = $busca->fetch_object()){
-		print_r($reg);
-		}
-	}
+	// fazer uma busca
+	//$busca = $banco->query("select * from generos");
+	//if(!$busca) {
+	//	echo "<p>A busca não aconteceu</p>";
+	// }
+	//else{
+	//	while($reg = $busca->fetch_object()){
+	//	print_r($reg);
+	//	}
+	//}
 ?>
--- index.php
+++ index.php
@@ -4,15 +4,43 @@
 	<title>Listagem de jogos</title>
 	<meta charset = "UTF-8"/>
 	<link rel="stylesheet" href="estilos/style.css" />
+	<?php
+		function thumb($arq){
+			$caminho = "fotos/$arq";
+			if(is_null($arq) || !file_exists($caminho)){
+				return "fotos/indisponivel.png";
+			} else {
+				return $caminho;
+			}
+		}
+	?>
 </head>
 <body>
+	<?php
+		require_once "includes/banco.php";
+	?>
 	<div id="corpo">
 		<h1>Escolha seu jogo</h1>
 		<table class="listagem">
-			<tr><td>Foto<td>Nome<td>Adm
-			<tr><td>Foto<td>Nome<td>Adm
-			<tr><td>Foto<td>Nome<td>Adm
-			<tr><td>Foto<td>Nome<td>Adm
+			<?php
+				$busca = $banco->query("select * from jogos order by nome");
+				if(!$busca) {
+					echo "<tr><td> A busca não aconteceu";
+				} else {
+					if ($busca->num_rows == 0){
+						echo "Não há jogos cadastrados";
+					} else {
+						while ($reg = $busca->fetch_object()){
+							$t = thumb($reg->capa);
+						echo "<tr>
+								<td><img class='mini' src='$t'/></td>
+								<td>" . $reg->nome . "</td>
+								<td>Adm</td>
+							</tr>";
+						}
+					}	
+				}
+			?>
 		</table>
 	</div>
 </body>
